ccbcf489cd15940ed4f74c8c9510e422862c9ef3
Heero Punjabi <heeropunjabi@gmail.com>
2022-08-09 21:23:12 +0530

    consuming widget

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CryptoList from "react-widget-poc/src/components/CryptoList";
+import Demo, { CryptoList, Demo2 } from "react-widget-poc";
 
 import logo from "./logo.svg";
 
@@ -12,7 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Demo />
         <CryptoList />
+        <Demo2 />
       </header>
     </div>
   );
